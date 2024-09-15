70f28e63a7387bfcb6d2839173669d76e3d22d33
tejasjadhav06 <jadhavtejas405@gmail.com>
2024-09-15 20:15:37 +0530

    Fixes some bugs.

--- JpaDemo/src/main/java/com/example/JpaDemo/controller/StudentController.java
+++ JpaDemo/src/main/java/com/example/JpaDemo/controller/StudentController.java
@@ -1,13 +1,26 @@
 package com.example.JpaDemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.JpaDemo.model.Student;
+import com.example.JpaDemo.service.StudentService;
 
 @RestController
 public class StudentController {
 
+	@Autowired
+	private StudentService service;
 	@GetMapping("/")
 	public String greet() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		return service.getAllStudents();
 	}
 }
--- JpaDemo/src/main/java/com/example/JpaDemo/service/StudentService.java
+++ JpaDemo/src/main/java/com/example/JpaDemo/service/StudentService.java
@@ -1,8 +1,21 @@
 package com.example.JpaDemo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.JpaDemo.model.Student;
+import com.example.JpaDemo.repository.StudentRepo;
 
 @Service
 public class StudentService {
+
+	@Autowired
+	private StudentRepo repo;
+	
+	public List<Student> getAllStudents() {
+		return repo.findAll();
+	}
 
 }
